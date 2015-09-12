c2fc0a0b3eaf5b89dfd67a160199b23f643c98a0
bruno <bruno.sofiato@gmail.com>
2015-09-12 16:20:07 -0300

    Ajustes de nomenclatura Inclusao de classe para implementacao do algoritmo "On the Fly^

--- exercicio1/src/main/java/br/usp/ime/mac5732/exercicio1/algoritmo/EquivalenceCheckinAlgorithm.java
+++ exercicio1/src/main/java/br/usp/ime/mac5732/exercicio1/algoritmo/EquivalenceCheckinAlgorithm.java
@@ -2,6 +2,6 @@ package br.usp.ime.mac5732.exercicio1.algoritmo;
 
 import br.usp.ime.mac5732.exercicio1.lts.LTS;
 
-public interface AlgoritmoEquivalencia {
+public interface EquivalenceCheckinAlgorithm {
   boolean isEquivalent(LTS op1, LTS op2, ProgressFeedbackListener listener);
 }
---
+++ exercicio1/src/main/java/br/usp/ime/mac5732/exercicio1/algoritmo/onthefly/OnTheFlyBehavioralEquivalenceCheckingAlgorithm.java
@@ -0,0 +1,12 @@
+package br.usp.ime.mac5732.exercicio1.algoritmo.onthefly;
+
+import br.usp.ime.mac5732.exercicio1.algoritmo.EquivalenceCheckinAlgorithm;
+import br.usp.ime.mac5732.exercicio1.algoritmo.ProgressFeedbackListener;
+import br.usp.ime.mac5732.exercicio1.lts.LTS;
+
+public class OnTheFlyBehavioralEquivalenceCheckingAlgorithm implements EquivalenceCheckinAlgorithm {
+  @Override
+  public boolean isEquivalent(LTS op1, LTS op2, ProgressFeedbackListener listener) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+}
