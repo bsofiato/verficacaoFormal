d1a6ca3974163b79700f8d665d9778f43fa9bf75
Rafael Leme Costa <lemerafael@gmail.com>
2015-09-20 16:26:37 -0300

    avanços Refinamentos Sucessivos

--- exercicio1/src/main/java/br/usp/ime/mac5732/exercicio1/algoritmo/refinamentossucessivos/RefinamentosSucessivos.java
+++ exercicio1/src/main/java/br/usp/ime/mac5732/exercicio1/algoritmo/refinamentossucessivos/RefinamentosSucessivos.java
@@ -1,6 +1,8 @@
 package br.usp.ime.mac5732.exercicio1.algoritmo.refinamentossucessivos;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 import br.usp.ime.mac5732.exercicio1.lts.Estado;
@@ -22,8 +24,9 @@ public class RefinamentosSucessivos {
 //	create a new node X12 as previously and add it to W.
 	
 	private LTS lts;
+	private Set<Set<Estado>> rho;
 	
-	private void refinementStep (Set<Estado> B, Set<Set<Estado>>W) {
+	private void simpleSplitterRefinement (Set<Set<Estado>> B, Set<Set<Set<Estado>>>W) {
 		Set<String> A = lts.getAlfabeto();
 //	 	Step 1. Remove the element B from W.
 		W.remove(B);
@@ -32,18 +35,47 @@ public class RefinamentosSucessivos {
 //	 Step 2. Compute the set I={x1|Ex Ep ^ x1 = x && Ta-1[B] != 0).
 	        Set<Estado> I;
 //	 TODO Copy the elements of B into a temporary set B’. 
-	        Set <Estado> B_ = B;
+//	        Set <Estado> B_ = B;
+	        Set <Estado> B1 = new HashSet<Estado>();
+	        Set <Estado> B2 = new HashSet<Estado>();
 //	 For each state p in Ta[B]
-		    for (Estado p : B_){
-//		    	if p pertence Ta-1[B]
-		    }
+		    for (Set<Estado> p_ : B){
+		    	for (Estado p : p_){
+			    	Map<String, Set<Estado>> entradas = p.getEntradas();
+			    	Set<Estado> estadosEntradas = entradas.get(a);
+			    	for (Estado est : estadosEntradas) {
+			    		if (est.equals(p)) {
 //	 move this p into a new class. (The elements of a same class are moved into the same new
-//	 class.) Make each new class point to its associated old class. During the scan of B’,
-//	 compute the map infob.
-	        
+//	 class.) Make each new class point to its associated old class. 
+			    			B1.add(est);
+			    		} else {
+			    			B2.add(est);
+		    			}
+		    		}
+		    	}
+		    }
+//	 if X2 != X1, add X1 to rho'
+		    if (!B1.equals(B2)) {
+		    	rho.add(B1);
+		    }
+//	 if X2 E W, and X2 is a simple splitter then add X1 to W
+		    Set<Set<Estado>> splitter = new HashSet<>();
+		    splitter.add(B2);
+		    
+	        if (W.contains(splitter) && B.size() == 1) {
+	        	splitter.remove(B2);
+	        	splitter.add(B1);
+	        	W.add(splitter);
+	        }
+//	  if X2 E W, and X2 is a leaf of a compound splitter then create a
+//	  compound splitter X12 with leaves X1 and X2 and add X12 to W
+//	  if X2 !E W, then create a
+//	  compound splitter X12 with leaves X1 and X2 and add X12 to W	        
+	        if ((W.contains(splitter) && B.size() > 1) || !W.contains(splitter)) {
+	        	splitter.add(B1);
+	        	W.add(splitter);
+	        }
 	     }
-			
-		
 	}
 	
 }
