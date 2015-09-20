9ee1e4ad442a8216da03a0e7c4bb49c4be565c2f
Rafael Leme Costa <lemerafael@gmail.com>
2015-09-20 00:26:52 -0300

    Tentativa algoritmo refinamentos sucessivos

--- exercicio1/src/main/java/br/usp/ime/mac5732/exercicio1/algoritmo/refinamentossucessivos/RefinamentosSucessivos.java
+++ exercicio1/src/main/java/br/usp/ime/mac5732/exercicio1/algoritmo/refinamentossucessivos/RefinamentosSucessivos.java
@@ -1,17 +1,13 @@
 package br.usp.ime.mac5732.exercicio1.algoritmo.refinamentossucessivos;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import br.usp.ime.mac5732.exercicio1.lts.Estado;
+import br.usp.ime.mac5732.exercicio1.lts.LTS;
 
 public class RefinamentosSucessivos {
 	
-//	 For each a in A, perform the following two steps:
-//	 Step 2. Compute the set I={x,13xEp A x, = x n T;‘[B] # 0).
-//	 Copy the elements of B into a temporary set B’. For each state p in Ta[B] move
-//	 this p into a new class. (The elements of a same class are moved into the same new
-//	 class.) Make each new class point to its associated old class. During the scan of B’,
-//	 compute the map infob.
 
 //	Step 3. Update p and W. After Step 2, each old class X contains the elements
 //	X - Ta[B]. For each X1, in I perform the following statements:
@@ -25,9 +21,28 @@ public class RefinamentosSucessivos {
 //	since the old class points to its father.) If X is not in W and X is not a leaf then
 //	create a new node X12 as previously and add it to W.
 	
+	private LTS lts;
+	
 	private void refinementStep (Set<Estado> B, Set<Set<Estado>>W) {
-//	 Step 1. Remove the element B from W.
+		Set<String> A = lts.getAlfabeto();
+//	 	Step 1. Remove the element B from W.
 		W.remove(B);
+//		For each a in A, perform the following two steps:
+	    for (String a : A){
+//	 Step 2. Compute the set I={x1|Ex Ep ^ x1 = x && Ta-1[B] != 0).
+	        Set<Estado> I;
+//	 TODO Copy the elements of B into a temporary set B’. 
+	        Set <Estado> B_ = B;
+//	 For each state p in Ta[B]
+		    for (Estado p : B_){
+//		    	if p pertence Ta-1[B]
+		    }
+//	 move this p into a new class. (The elements of a same class are moved into the same new
+//	 class.) Make each new class point to its associated old class. During the scan of B’,
+//	 compute the map infob.
+	        
+	     }
+			
 		
 	}
 	
