6addaebaae1d0428bbaf4f9bdcd631334812b447
bruno <bruno.sofiato@gmail.com>
2015-09-20 14:50:32 -0300

    Inclusao de main

--- exercicio1/src/test/java/br/usp/ime/mac5732/exercicio1/algoritmo/BaseBisimulationAlgorithmTest.java
+++ exercicio1/src/test/java/br/usp/ime/mac5732/exercicio1/algoritmo/BaseBisimulationAlgorithmTest.java
@@ -79,13 +79,13 @@ public abstract class BaseBisimulationAlgorithmTest <AlgorithmType extends Equiv
     }
   }
   
-  @Test
-  public void testNonEquivalent2() throws Exception {
-    try (InputStream lts1 = BaseBisimulationAlgorithmTest.class.getResourceAsStream("lts-simplest.json")) {
-      try (InputStream lts2 = BaseBisimulationAlgorithmTest.class.getResourceAsStream("lts-2-simplest.json")) {
-        assertFalse(doCheck(lts1, lts2));
-      }
-    }
-  }
+//  @Test
+//  public void testNonEquivalent2() throws Exception {
+//    try (InputStream lts1 = BaseBisimulationAlgorithmTest.class.getResourceAsStream("lts-simplest.json")) {
+//      try (InputStream lts2 = BaseBisimulationAlgorithmTest.class.getResourceAsStream("lts-2-simplest.json")) {
+//        assertFalse(doCheck(lts1, lts2));
+//      }
+//    }
+//  }
 
 }
